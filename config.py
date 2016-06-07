48d0acec5d65d25dae87185e6471f44ebd9bf4cb
andor-z <zyfsta@outlook.com>
2016-06-07 21:59:57 +0800

    +

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://*:*@localhost/cm_web_dev'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
